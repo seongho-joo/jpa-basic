86d97fbd609ae10fc953bda915c2e6de655f3721
seongho-joo <hazzyseong@gmail.com>
2022-04-14 16:14:54 +0900

    CHORE: 요구하상 분석과 기본 매핑

---
+++ src/main/java/jpa/jpabasic/domain/OrderItem.java
@@ -0,0 +1,25 @@
+package jpa.jpabasic.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    private Long orderId;
+    private Long itemId;
+
+    private int orderPrice;
+    private int count;
+}
